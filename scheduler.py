cb28efa625dfe5bf4665ca36a10c9d260e4ceabe
Dannyyang0329 <f74094067@gs.ncku.edu.tw>
2023-05-25 21:53:36 +0800

    docs: finish all project

--- scheduler.py
+++ scheduler.py
@@ -22,8 +22,6 @@ class Scheduler:
             aperiodic_jobs = get_job_info(schedule["Aperiodic"], "Aperiodic")
             sporadic_jobs = get_job_info(schedule["Sporadic"], "Sporadic")
 
-            # hard_jobs = sorted(periodic_jobs+sporadic_jobs, key=lambda job: int(job[1]))
-            # soft_jobs = sorted(aperiodic_jobs, key=lambda job: int(job[0]))
             sorted_periodic = sorted(periodic_jobs, key=lambda job: int(job[1]))
             sorted_sporadic = sorted(sporadic_jobs, key=lambda job: int(job[1]))
             sorted_aperiodic = sorted(aperiodic_jobs, key=lambda job: int(job[0]))
@@ -34,21 +32,6 @@ class Scheduler:
 
             # optimal off-line scheduling in 100 time units
             reject_list = []
-            # while True:
-            #     is_finish = True
-            #     for h_job in hard_jobs:
-            #         res, new_time_line = fillin_schedule(h_job, time_line)
-            #         if res == True:
-            #             time_line = new_time_line
-            #         else:
-            #             reject_list.append(str(f"{h_job[3][0]} {h_job[3][1:]} -1 -1 Reject"))
-            #             if h_job[3][0] == "P":
-            #                 time_line = empty_time_line.copy()
-            #                 hard_jobs = remove_reject_task(h_job[3], hard_jobs)
-            #                 is_finish = False
-            #                 break
-            #     if is_finish == True:
-            #         break
             while True:
                 is_finish = True
                 for p_job in sorted_periodic:
@@ -78,35 +61,32 @@ class Scheduler:
                 else:
                     reject_list.append(str(f"{a_job[3][0]} {a_job[3][1:]} -1 -1 Reject"))
 
-            # for s_job in soft_jobs:
-            #     res, new_time_line = fillin_schedule(s_job, time_line)
-            #     if res == True:
-            #         time_line = new_time_line
-            #     else:
-            #         reject_list.append(str(f"{s_job[3][0]} {s_job[3][1:]} -1 -1 Reject"))
-
             for reject_job in reject_list:
                 print(reject_job)
                 self.output_list.append(reject_job + "\n")
 
             tmp = show_schedule(time_line)
             self.output_list += tmp
+
+            # show statistics of "Periodic", "Aperiodic", "Sporadic"
+            reject_num = [0, 0, 0]
+            for reject_job in reject_list:
+                if reject_job[0] == "P":
+                    reject_num[0] += 1
+                elif reject_job[0] == "A":
+                    reject_num[1] += 1
+                elif reject_job[0] == "S":
+                    reject_num[2] += 1
+            periodic_rate = reject_num[0] / len(schedule["Periodic"])
+            aperiodic_rate = reject_num[1] / len(aperiodic_jobs)
+            sporadic_rate = reject_num[2] / len(sporadic_jobs)
+            rates = str(f"{periodic_rate} {aperiodic_rate} {sporadic_rate}")
+            print(rates)
+            self.output_list.append(rates + "\n")
+
             if len(self.schedules)-1 != idx:
                 print("FINISH")
                 self.output_list.append("FINISH")
-
-            # print the detail of the time line
-            # for i in range(0, 10):
-            #     print(i, end=": ")
-            #     for j in range(i*10, (i+1)*10):
-            #         print("%5s" % time_line[j], end=" ")
-            #     print("")
-
-            # show statistics of "Periodic", "Aperiodic", "Sporadic"
-            # periodic_rate = reject_num["Periodic"] / len(periodic_jobs)
-            # aperiodic_rate = reject_num["Aperiodic"] / len(aperiodic_jobs)
-            # sporadic_jobs = reject_num["Sporadic"] / len(sporadic_jobs)
-            # print(f"{periodic_rate} {aperiodic_rate} {sporadic_jobs}")
 
         # end of output
         print("-1")
